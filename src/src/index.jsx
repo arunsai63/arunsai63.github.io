d44d4f14f416308832ad105c8a3ad2a5ccae10d2
veneno <arun.munaganti@ggktech.com>
2019-09-18 11:35:02 +0530

    modified src/index.jsx

--- src/src/index.jsx
+++ src/src/index.jsx
@@ -7,10 +7,28 @@ class Main extends React.Component {
             "Name": "Sai Arun Veneno",
             "Email": "arunsai63@gmail.com",
             "projects": [
-                "project1",
-                "project2",
-                "project3"
+                "Handwritten Character Recognition",
+                "Three Tier Architecture",
+                "Image Store API",
+                "..."
             ],
+            "Frontend": [
+                "HTML", "CSS", "SASS/SCSS", "Bootstrap",
+                "ReactJs"
+            ],
+            "Backend": [
+                "C", "C++", "C#", "Java", "NodeJs", "Php", "Python"
+            ],
+            "frameworks": [
+                "Django", "Bottle",
+                ".NET", ".NET core", "ASP.NET", "Web API", "ADO.NET", "Entity Framework",
+                "Express"
+            ],
+            "Database": [
+                "SQL Server", "PostgreSQL", "Oracle", "MySql",
+                "MongoDb",
+                "SQlite"
+            ]
         }
         return <pre>{JSON.stringify(details, null, 4)}</pre>
     }
